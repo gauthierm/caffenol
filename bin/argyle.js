c966bf5d3f0918f01352ebf59c8fc1deeaf3ca19
Michael Gauthier <mike.gauthier@gmail.com>
2015-05-13 01:17:01 -0300

    Implement CLI for processing jobs from queue.

--- bin/argyle.js
+++ bin/argyle.js
@@ -5,3 +5,74 @@
 var chalk = require('chalk');
 var argv = require('minimist')(process.argv.slice(2));
 var kue = require('kue');
+var cliPackage = require('../package');
+var util = require('../lib/util');
+
+var debugFlag = argv.d || argv.debug;
+var versionFlag = argv.v || argv.version;
+var port = argv.port || 3000;
+var helpFlag = argv.h || argv.help;
+
+if (versionFlag) {
+  var name = cliPackage.name;
+  util.log(
+    name.substring(0, 1).toUpperCase() + name.substring(1),
+    'CLI version',
+    cliPackage.version
+  );
+  process.exit(0);
+}
+
+if (helpFlag) {
+  util.log(cliPackage.description);
+  util.log();
+  util.log('Usage:');
+  util.log('  ', cliPackage.name);
+  util.log();
+  util.log('Options:');
+  util.log('  -d, --debug           Show extra debug output.');
+  util.log('  -h, --help            Show this help and exit.');
+  util.log('  -p port, --port=port  Port to use for queue UI.');
+  util.log('  -v, --version         Show the version and exit.');
+  util.log();
+  process.exit(0);
+}
+
+var queue = kue.createQueue();
+var concurrentJobs = 4;
+
+process.once('SIGINT', function(sig) {
+  if (queue.client.connected) {
+    queue.shutdown(function(err) {
+      if (err) {
+        util.error('Shutdown with error', err);
+        process.exit(1);
+      } else {
+        util.log('Good bye!');
+        process.exit(0);
+      }
+    }, 5000);
+  } else {
+    util.log('Good bye!');
+    process.exit(0);
+  }
+});
+
+queue.on('error', function(err) {
+  if (/ECONNREFUSED/.test(err.message)) {
+    util.error(chalk.red(err.message));
+    process.exit(1);
+  } else {
+    util.error(chalk.red(err.message));
+  }
+})
+
+queue.process('process-photo', concurrentJobs, function(job, done) {
+  util.debug('Got a job!');
+  console.dir(job.data);
+  util.debug('Job done');
+  done();
+});
+
+kue.app.listen(port);
+util.log('Started on port', port);
